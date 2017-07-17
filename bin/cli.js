db23bc802ae762e95015f23b56e6bf20eaf5cfaa
roccomuso <rocco@hackerstribe.com>
2017-07-17 16:08:15 +0200

    code standardized

--- bin/cli.js
+++ bin/cli.js
@@ -3,8 +3,7 @@
 const inquirer = require('inquirer')
 const questions = require('../lib/questions')
 
-
 inquirer.prompt(questions).then(function (answers) {
-  console.log('\nOutput receipt:');
-  console.log(JSON.stringify(answers, null, '  '));
-});
+  console.log('\nOutput receipt:')
+  console.log(JSON.stringify(answers, null, '  '))
+})
