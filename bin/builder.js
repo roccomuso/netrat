dcca4a157b75bf4aa38a348e11fb57647a281c92
roccomuso <rocco@hackerstribe.com>
2017-07-20 16:14:29 +0200

    add debug module + builder sketch

---
+++ bin/builder.js
@@ -0,0 +1,7 @@
+const debug = require('debug')('netrat')
+
+function builder (options) {
+  
+}
+
+module.exports = builder
--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 'use strict'
+const debug = require('debug')('netrat')
 const inquirer = require('inquirer')
 const questions = require('../lib/questions')
+const builder = require('../lib/builder')
 
 inquirer.prompt(questions).then(function (answers) {
-  console.log('\nOutput receipt:')
-  console.log(JSON.stringify(answers, null, '  '))
+  debug('\nOutput receipt:')
+  debug(JSON.stringify(answers, null, '  '))
+  builder(answers)
 })
