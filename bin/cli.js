3136559b40a0686ec927de3d64b1e1e75e1b40d6
roccomuso <rocco@hackerstribe.com>
2017-07-26 15:17:17 +0200

    add ascii logo

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,9 @@ const debug = require('debug')('netrat')
 const inquirer = require('inquirer')
 const questions = require('../lib/questions')
 const builder = require('../lib/builder')
+const LOGO = require('../other/logo.js')
+
+console.log(LOGO)
 
 inquirer.prompt(questions).then(function (answers) {
   debug('\nOutput receipt:')
